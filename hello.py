e35c0b9c3185acc4fe38c30566ceeb84d12d437d
Saminul <s.islamsamin@mail.utoronto.ca>
2021-09-26 18:48:36 -0400

    Acitvity 4 - Step 2: implemented email field

--- hello.py
+++ hello.py
@@ -4,7 +4,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+from wtforms.fields.html5 import EmailField
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password'
@@ -14,6 +15,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -35,10 +37,13 @@ def index():
 
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you changed your email!')
 
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
